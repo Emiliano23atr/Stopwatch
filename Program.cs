3ba135aaa6e83acb144845fcf263901d0ece337c
Emiliano23atr <joaovitoremiliano5@gmail.com>
2022-12-21 21:09:14 -0300

    Metodo Start criado

--- Program.cs
+++ Program.cs
@@ -5,4 +5,14 @@ class Program
     {
         Console.WriteLine("Hello, World!");
     }
+    static void Start()
+    {
+        int time = 10;
+        int currentTime = 0;
+
+        while (currentTime != time)
+        {
+            currentTime++;
+        }
+    }
 }

d93f8358c1bc41425e66943df979c05b40583a5e
Emiliano23atr <joaovitoremiliano5@gmail.com>
2022-12-22 22:40:20 -0300

    Calculando o tempo

--- Program.cs
+++ Program.cs
@@ -17,8 +17,19 @@ class Program
         Console.WriteLine("Quanto tempo deseja contar?");
 
         string data = Console.ReadLine().ToLower();
-        char type = char.Parse(data.Substring(data.Length-1, 1));
-        Console.WriteLine(type);
+        char type = char.Parse(data.Substring(data.Length - 1, 1));
+        int time = int.Parse(data.Substring(0 , data.Length - 1));
+        int multiplier = 1;
+
+        if(type == 'm')
+        {
+            multiplier = 60;
+        }
+        if(time == 0)
+        {
+            System.Environment.Exit(0);
+        }
+        Start(time * multiplier);
     }
 
     static void Start(int time)
@@ -36,5 +47,6 @@ class Program
         Console.Clear();
         Console.WriteLine("Stopwatch finalizado");
         Thread.Sleep(2500);
+        Menu();
     }
 }

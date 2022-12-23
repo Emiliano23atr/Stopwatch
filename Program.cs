48921ee4d12f5e60b6ec6876b65cf81929c0ee69
Emiliano23atr <joaovitoremiliano5@gmail.com>
2022-12-22 22:16:45 -0300

    Substring

--- Program.cs
+++ Program.cs
@@ -17,6 +17,8 @@ class Program
         Console.WriteLine("Quanto tempo deseja contar?");
 
         string data = Console.ReadLine().ToLower();
+        char type = char.Parse(data.Substring(data.Length-1, 1));
+        Console.WriteLine(type);
     }
 
     static void Start(int time)
